62b39929d0b9d0d8ba24be791a8dbc64e377496a
vifo07 <38468212+vifo07@users.noreply.github.com>
2021-05-27 23:58:53 -0500

    Add files via upload

---
+++ qa-automation-academy/src/page_model/data/Data.js
@@ -0,0 +1,37 @@
+export const URLS= {
+    BASE_PAGE:'https://www.saucedemo.com/'
+}
+export const CREDENTIALS= {
+    VALID_USER:{
+        USERNAME:'standard_user',
+        PWD:'secret_sauce'
+    },
+    INVALID_USER:{
+            USERNAME:'standard_user',
+            PWD:'abcd'
+        }
+    
+} 
+export const VARIABLES={
+    PERSONAL_DATA:{
+        FIRSTNAME:'Alejandro',
+        LASTNAME:'Alvarado',
+        ZIPCODE:'5887'
+    },
+    PRODUCTSTOADD:2,
+    SINGLEPRODUCT:1
+ 
+}
+export const MESSAGES= {
+    INVALID_LOGIN_MSG:'Username and password do not match any user in this service',
+    FIRSTNAME_MISSING_MSG:'Error: First Name is required',
+    PURCHASE_COMPLETED_MSG:'THANK YOU FOR YOUR ORDER',
+    HEADERS:{
+        PRODUCT_PAGE:'Product',
+        YOURCART_PAGE:'Your Cart',
+        OVERVIEW_PAGE:'Checkout: Overview'
+        
+
+    }
+   
+}
